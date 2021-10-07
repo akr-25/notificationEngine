011085485cd33be4f36619afbb76be16727f4d49
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2021-10-08 01:50:40 +0530

    mapping defined

--- backend/notificationEngine/__init__.py
+++ backend/notificationEngine/__init__.py
@@ -1,33 +1,39 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, login_manager
 from notificationEngine.config import Config
 
 
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+# db = SQLAlchemy()
+# bcrypt = Bcrypt()
+# login_manager = LoginManager()
 
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
+# def create_app(config_class=Config):
+#     app = Flask(__name__)
+#     app.config.from_object(Config)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
+#     db.init_app(app)
+#     bcrypt.init_app(app)
+#     login_manager.init_app(app)
 
-    from notificationEngine.users.routes import users
-    from notificationEngine.role.routes import role
-    from notificationEngine.admin.routes import admin
-    from notificationEngine.trigger.routes import trigger
-    from notificationEngine.notification.routes import notification
+    # from notificationEngine.users.routes import users
+    # from notificationEngine.role.routes import role
+    # from notificationEngine.admin.routes import admin
+    # from notificationEngine.trigger.routes import trigger
+    # from notificationEngine.notification.routes import notification
 
-    app.register_blueprint(users)
-    app.register_blueprint(role)
-    app.register_blueprint(admin)
-    app.register_blueprint(trigger)
-    app.register_blueprint(notification)
+    # app.register_blueprint(users)
+    # app.register_blueprint(role)
+    # app.register_blueprint(admin)
+    # app.register_blueprint(trigger)
+    # app.register_blueprint(notification)
 
-    return app
+    # return app
+
+app = Flask(__name__)
+# app.config.from_object(Config)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost:3306/buildathon"
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
--- backend/notificationEngine/models.py
+++ backend/notificationEngine/models.py
@@ -1,65 +1,106 @@
 from datetime import datetime
 from flask import current_app
 from notificationEngine import db, login_manager
-from flask_login import UserMixin
+# from flask_login import UserMixin
+from sqlalchemy.dialects.mysql import JSON
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# subs = db.Table('subs',
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'), nullable = False),
+#     db.Column('noti_id', db.Integer, db.ForeignKey(
+#         'notification.noti_id'),  nullable=False),
+#     db.Column('user_config', JSON)
+# )
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True, nullable=False)
+class User_Notif(db.Model):
+    __tablename__ = 'mapping'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    noti_id = db.Column(db.Integer, db.ForeignKey('notification.noti_id'), primary_key=True)
+
+    user_config = db.Column(JSON)
+    user_response = db.Column(JSON)
+
+    user = db.relationship('User', back_populates = "notifications")
+    notification = db.relationship('Notification', back_populates = "users")
+
+    def __repr__(self):
+        return f"Map('{self.user_id}', '{self.noti_id}')"
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    history = db.Column(db.Text)
-    # roles = db.relationship()
-    # notification = db.relationship()
+    history = db.Column(JSON)
+    roles = db.Column(db.String(20))
+
+    #relations
+    notifications = db.relationship('User_Notif',  back_populates = "user")
+
+    #meta data
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
     def __repr__(self):
-        return f"User('{self.name}', '{self.email}', '{self.roles}')"
-
-
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    # notification = db.relationship()
-
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}', '{self.roles}')"
-
+        return f"User('{self.name}', '{self.email}')"
+        # return f"User('{self.name}', '{self.email}', '{self.roles}')"
 
 class Notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    noti_id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20), nullable = False)
     title = db.Column(db.String(100), nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    createdBy = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    configuration = db.Column(db.Text)
+    configuration = db.Column(JSON)
     isAdmin = db.Column(db.Boolean, nullable = False, default = False)
     role = db.Column(db.String(20), nullable = False, default = "NA")
 
+    #relations
+    users = db.relationship('User_Notif', back_populates = "notification")
+
+    #meta data
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # createdBy = db.relationship(db.Integer, db.ForeignKey('user.user_id'))
+
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Notification('{self.type}', '{self.content}', '{self.role}')"
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    # members = db.relationship()
+# class Trigger(db.Model):
+#     trigger_id = db.Column(db.Integer, primary_key = True)
+#     type = db.Column(db.String(20), nullable = False)
+#     # createdBy = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     create_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     configuration = db.Column(JSON)
+#     isAdmin = db.Column(db.Boolean, nullable=False, default=False)
 
-class Trigger(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    type = db.Column(db.String(20), nullable = False)
-    createdBy = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False,
-                             default=datetime.utcnow)
-    configuration = db.Column(db.Text)
-    responses = db.Column(db.Text)
-    isAdmin = db.Column(db.Boolean, nullable=False, default=False)
-    # notification = db.relationship()
+#     # notification = db.relationship()
+
+#     def __repr__(self):
+#        return f"Trigger('{self.type}', '{self.content}', '{self.role}')"
+
+# class Admin(db.Model, UserMixin):
+#     admin_id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(30), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(60), nullable=False)
+#     # notification = db.relationship()
+
+#     def __repr__(self):
+#         return f"User('{self.name}', '{self.email}', '{self.roles}')"
+
+
+
+
+#     def __repr__(self):
+#         return f"Post('{self.title}', '{self.date_posted}')"
+
+# class Role(db.Model):
+#     role_id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(30), nullable = False)
+#     # members = db.relationship()
+
+
 
