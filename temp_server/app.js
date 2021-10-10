f3243f35d71b9b29fa9f97d6a177397b5b28c6c1
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2021-10-11 01:23:14 +0530

    created temp_server

---
+++ temp_server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
+
+app = express();
+app.use(cors());
+app.use(morgan("common"));
+
+app.get("/api/test", (req, res) => {
+    return res.status(200).send("Connection Established");
+})
+
+app.listen(3030, () => {
+    console.log("Listening on port 3030");
+})
